27b4a56e7d22f6ba00c53fe4345cacf4b9bf25a4
slince-zero <1711257704@qq.com>
2023-07-05 10:20:04 +0800

    单文件写法

---
+++ 10_单文件组件/School.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="demo">
+        <h2>学校名称: {{ name }}</h2>
+        <h2>学校地址: {{ add }}</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'School',
+    data() {
+        return {
+            name: '北京大学',
+            add: '北京'
+        }
+    }
+}
+</script>
+
+<style>
+.demo{
+    background-color: orange;
+}
+</style>
---
+++ 10_单文件组件/Student.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h2>学生姓名: {{ name }}</h2>
+        <h2>年龄: {{ age }}</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'Student',
+    data() {
+        return {
+            name: '小明',
+            add: '22'
+        }
+    }
+}
+</script>
+
+<style></style>
